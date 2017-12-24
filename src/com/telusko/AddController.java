0eaa56e9c04733a565c56e1821789440a9f8e9bb
Subhankar <subho10.in@gmail.com>
2017-12-25 03:09:01 +0530

    Trying to redirect to jsp

--- src/com/telusko/AddController.java
+++ src/com/telusko/AddController.java
@@ -1,14 +1,26 @@
 package com.telusko;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AddController {
 	
 	@RequestMapping("/add")
-	public void add() {
-		System.out.println("I am here. Lol");
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
+//		System.out.println("I am here. Lol");
+		int i = Integer.parseInt(request.getParameter("t1"));
+		int j = Integer.parseInt(request.getParameter("t2"));
+		System.out.println("i: "+i+ " j:"+j);
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("display.jsp");
+		mv.addObject("re", (i+j));
+		
+		return mv;
 	}
 
 }
